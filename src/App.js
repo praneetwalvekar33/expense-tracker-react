3fea5e2ab999f044e2a581d231fc3f606d39ca54
praneetwalvekar33 <praneetwalvekar@gmail.com>
2023-01-25 22:16:06 +0530

    feat- form to add new transactions with required validation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import Header from './components/header/Header';
 import Card from './components/card/Card';
+import AddNewTransactionForm from './components/addNewTransactionFrom/AddNewTransactionForm';
 import './App.css'
 
 const App = () => {
 
     return (
-        <div>
+        <div className="app">
             <Header/>
             <div className="cards-container">
                 <Card header="Total Balance" amount={45000} balOrExp={true}/>
                 <Card header="Total Expense" amount={3500} balOrExp={false}/>
             </div>
+
+            <AddNewTransactionForm className="new-transaction" />
+
+            <div>hello</div>
         </div>
     )
 }
