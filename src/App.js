99f3989d2a819c466a4b35e6480c7c6271ba5d8f
praneetwalvekar33 <praneetwalvekar@gmail.com>
2023-02-01 02:15:25 +0530

    component-created user entry component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Card from './components/card/Card';
 import AddNewTransactionForm from './components/addNewTransactionFrom/AddNewTransactionForm';
 import TransactionEntryHeader from './components/transactionEntryHeader/TransactionEntryHeader';
+import UserTransactionEntry from './components/userTransactionEntry/UserTransactionEntry';
 import './App.css'
 
 const App = () => {
@@ -19,6 +20,9 @@ const App = () => {
             <AddNewTransactionForm className="new-transaction" />
 
             <TransactionEntryHeader />
+
+            <UserTransactionEntry prop='1'/>
+            <UserTransactionEntry prop='0'/>
         </div>
     )
 }
