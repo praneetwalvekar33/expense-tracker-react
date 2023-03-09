26a31d090d22c12c8812bbd6aadb4e8d45a71f20
praneetwalvekar33 <praneetwalvekar@gmail.com>
2023-03-09 20:25:16 +0530

    style-improved the style for user transaction list

--- src/components/transactionEntryHeader/TransactionEntryHeader.jsx
+++ src/components/transactionEntryHeader/TransactionEntryHeader.jsx
@@ -5,11 +5,11 @@ import './TransactionEntryHeader.css';
 const TransactionEntryHeader = ()=>{
     return(
         <div className="entry-header-container ">
-            <div className="color">No</div>
-            <div className="color name-margin">Name</div>
-            <div className="color margin-right">Date</div>
-            <div className="color margin-right">Type</div>
-            <div className="color margin-right">Amount</div>
+            <div className="color width align-text">No</div>
+            <div className="color width-name align-text">Name</div>
+            <div className="color width-date align-text">Date</div>
+            <div className="color width align-text">Type</div>
+            <div className="color width align-text">Amount</div>
         </div>
     )
 }
--- src/components/userTransactionEntry/UserTransactionEntry.jsx
+++ src/components/userTransactionEntry/UserTransactionEntry.jsx
@@ -5,11 +5,11 @@ import './UserTransactionEntry.css';
 const UserTransactionEntry = ({transaction,num}) => {
     return (
         <div className="user-entry-container" style={transaction.transactionType===1?{borderLeft: "12px solid #107100"}:{borderLeft:"12px solid #B00000"}}>
-            <div className="color">{num}</div>
-            <div className="color name-margin">{transaction.transactionName}</div>
-            <div className="color margin-right">{transaction.transactionDate}</div>
-            <div className="color margin-right">{transaction.transactionType}</div>
-            <div className="color margin-right">{transaction.transactionAmount}</div>
+            <div className="color width align-text">{num}</div>
+            <div className="color width-name align-text">{transaction.transactionName}</div>
+            <div className="color width-date align-text">{transaction.transactionDate}</div>
+            <div className="color width align-text">{transaction.transactionType}</div>
+            <div className="color width align-text">{transaction.transactionAmount}</div>
         </div>
     )
 }
