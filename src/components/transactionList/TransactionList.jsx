6c4ae0fed2ba4183583904a6dbf9670605472020
praneetwalvekar33 <praneetwalvekar@gmail.com>
2023-03-17 14:10:43 +0530

    feat- display loading and error message for the transaction list

--- src/components/transactionList/TransactionList.jsx
+++ src/components/transactionList/TransactionList.jsx
@@ -3,10 +3,28 @@ import { useQuery } from "@tanstack/react-query";
 import { getTransactionData } from '../../utils/apiCallUtility';
 
 import UserTransactionEntry from '../userTransactionEntry/UserTransactionEntry';
+import "./TransactionList.css";
+
 
 const TransactionList = () =>{
-    const {data} = useQuery(["transactionData"],()=> getTransactionData());
+    const {data,isLoading,error} = useQuery(["transactionData"],()=> getTransactionData());
     let no = 0;
+
+    if(isLoading){
+        return(
+            <div className="message" style={{color:"#107100"}}>
+                Please wait loading...
+            </div>
+        );
+    }
+
+    if(error){
+        return(
+            <div className="message" style={{color:"#B00000"}}>
+                An error occured while loading the list.
+            </div>
+        )
+    }
 
     return(
         <div>
